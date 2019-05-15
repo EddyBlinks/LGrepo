2ce561aec980051fac48291106e96c40e49c4cb3
Edward Mends <admin@MacBook-Air-de-admin.local>
2019-05-15 18:30:30 +0200

    new files for Lutz und Grob

---
+++ Autor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace OOPintermediate
+{
+    public class Autor
+    {
+
+        string vName, nName;
+        DateTime dob, dod;
+       
+
+        public Autor(string vName, string nName, DateTime dob, DateTime dod)
+        {
+            this.vName = vName;
+            this.nName = nName;
+            this.dob = dob;
+            this.dod = dod;
+
+
+        }
+
+        public void AutoInformaationen()
+        {
+            Console.WriteLine("Vorname: " + vName);
+            Console.WriteLine("Nachname: " + nName);
+            Console.WriteLine("Geburts Datum: " + dob.ToShortDateString());
+            Console.WriteLine("Gestorben am: " + dod.ToShortDateString());
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OOPintermediate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Autor auto1 = new Autor("Billy", "Goat", new DateTime(1946,2,4), new DateTime(1990,9,4));
+            auto1.AutoInformaationen();
+
+
+            Console.ReadKey();
+        }
+    }
+}
